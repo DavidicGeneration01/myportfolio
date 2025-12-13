3b54a9bb493703dad948a538f8aed3cf5c9cceed
davidicgeneration <dalade@byupathway.edu>
2025-12-13 10:27:50 +0100

    feat: Add Toolkit section
    Introduces the new Toolkit section to the application, enhancing the display of development tools and technologies used by David O. Alade. Updates import maps in index.html to reflect necessary dependencies and their versions for seamless integration of new components.

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Toolkit from './components/Toolkit';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -15,6 +16,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Toolkit />
         <Skills />
         <Projects />
         <Contact />
